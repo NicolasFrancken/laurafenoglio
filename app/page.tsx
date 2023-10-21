bbdf1f078afec5bb9776a41936a9d443ad75073d
NicolasFrancken <franckennicolas@gmail.com>
2023-10-20 21:00:39 -0300

    Main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,165 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
+import {
+  MdWhatsapp,
+  MdOutlineMailOutline,
+  MdLocationPin,
+  MdPhone,
+  MdOutlineCalendarMonth,
+  MdOutlineAccessTime,
+} from "react-icons/md";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div className="h-20 px-20 bg-background/90 flex justify-between items-center fixed top-0 left-0 right-0 border-b-2 border-foreground/20">
+        <Link href={"#main"}>
+          <Image src={"/logo.png"} alt="logo" width={200} height={50} />
+        </Link>
+        <nav className="flex ">
+          <Link
+            href={"#proyectos"}
+            className="px-4 py-2 rounded-lg text-[#7b797a] hover:text-[#812962] transition-colors ease-linear duration-100 font-semibold"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Proyectos
+          </Link>
+          <Link
+            href={"#nosotros"}
+            className="px-4 py-2 rounded-lg text-[#7b797a] hover:text-[#812962] transition-colors ease-linear duration-100 font-semibold"
+          >
+            Nosotros
+          </Link>
+          <Link
+            href={"#contacto"}
+            className="px-4 py-2 rounded-lg text-[#7b797a] hover:text-[#812962] transition-colors ease-linear duration-100 font-semibold "
+          >
+            Contacto
+          </Link>
+        </nav>
+      </div>
+      <section
+        id="main"
+        className="min-h-screen px-20 py-40 background1 flex flex-col justify-center items-center gap-5"
+      >
+        <Link
+          href={
+            "https://api.whatsapp.com/send/?phone=%2B542944959344&text=Hola%2C+tengo+una+consulta%21&type=phone_number&app_absent=0"
+          }
+          className="fixed bottom-0 right-0 m-6 hover:opacity-70 transition-opacity ease-linear duration-100"
+        >
+          <Image
+            src={"/WhatsApp.webp"}
+            alt="WhatsApp Logo"
+            width={70}
+            height={70}
+          />
+        </Link>
+        <div className="px-10 py-5 bg-background/90 w-[670px]">
+          <h1 className="text-6xl font-bold text-center">
+            EMPRENDIMIENTOS <br /> INMOBILIARIOS
+          </h1>
+          <p className="text-center font-bold text-2xl">
+            EN LA PATAGONIA ARGENTINA
+          </p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="px-10 py-5 bg-background/90 w-[670px]">
+          <p className="text-center font-semibold text-2xl">
+            Desarrollo de loteos abiertos, barrios cerrados, edificios, locales
+            comerciales y departamentos
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </div>
+      </section>
+      <section id="proyectos" className="min-h-screen px-20 py-[120px]">
+        <h2 className="font-bold text-4xl uppercase mb-10">
+          Nuestros Proyectos
+        </h2>
+        <div className="flex flex-col gap-10">
+          <div className="flex gap-10">
+            <div className="w-1/2 h-[400px] bg-foreground"></div>
+            <div className="w-1/2 h-[400px] bg-foreground"></div>
+          </div>
+          <div className="flex gap-10">
+            <div className="w-1/2 h-[400px] bg-foreground"></div>
+            <div className="w-1/2 h-[400px] bg-foreground"></div>
+          </div>
+          <div className="flex gap-10">
+            <div className="w-1/2 h-[400px] bg-foreground"></div>
+            <div className="w-1/2 h-[400px] bg-foreground"></div>
+          </div>
+          <div className="flex gap-10">
+            <div className="w-1/2 h-[400px] bg-foreground"></div>
+            <div className="w-1/2 h-[400px] bg-foreground"></div>
+          </div>
+        </div>
+      </section>
+      <section
+        id="contacto"
+        className="min-h-screen px-20 py-[120px] pb-[40px] background2 flex flex-col justify-center items-center gap-5"
+      >
+        <div className="bg-background/90 px-10 py-10 flex flex-col justify-center items-center w-[900px]">
+          <h2 className="font-bold text-4xl uppercase mb-5">Contactanos</h2>
+          <div className="flex flex-col gap-3">
+            <Link
+              href={
+                "https://api.whatsapp.com/send/?phone=%2B542944959344&text=Hola%2C+tengo+una+consulta%21&type=phone_number&app_absent=0"
+              }
+              className="font-semibold text-xl hover:text-[#812962] transition-colors ease-linear duration-100"
+            >
+              <MdWhatsapp className="inline w-7 h-7 mb-1 mr-1" /> Escribinos por
+              WhatsApp
+            </Link>
+            <Link
+              href={
+                "https://api.whatsapp.com/send/?phone=%2B542944959344&text=Hola%2C+tengo+una+consulta%21&type=phone_number&app_absent=0"
+              }
+              className="font-semibold text-xl hover:text-[#812962] transition-colors ease-linear duration-100"
+            >
+              <MdOutlineMailOutline className="inline w-7 h-7 mb-1 mr-1" />{" "}
+              Mandanos un Mail
+            </Link>
+            <Link
+              href={
+                "https://api.whatsapp.com/send/?phone=%2B542944959344&text=Hola%2C+tengo+una+consulta%21&type=phone_number&app_absent=0"
+              }
+              className="font-semibold text-xl hover:text-[#812962] transition-colors ease-linear duration-100"
+            >
+              <MdPhone className="inline w-7 h-7 mb-1 mr-1" /> Llamanos
+            </Link>
+          </div>
+        </div>
+        <div className="bg-background/90 px-10 py-10 flex flex-col justify-center items-center w-[900px]">
+          <h2 className="font-bold text-4xl uppercase mb-5">Horarios</h2>
+          <div className="flex flex-col gap-3">
+            <Link
+              href={
+                "https://api.whatsapp.com/send/?phone=%2B542944959344&text=Hola%2C+tengo+una+consulta%21&type=phone_number&app_absent=0"
+              }
+              className="font-semibold text-xl hover:text-[#812962] transition-colors ease-linear duration-100"
+            >
+              <MdOutlineCalendarMonth className="inline w-7 h-7 mb-1 mr-1" />{" "}
+              Lunes a Viernes
+            </Link>
+            <Link
+              href={
+                "https://api.whatsapp.com/send/?phone=%2B542944959344&text=Hola%2C+tengo+una+consulta%21&type=phone_number&app_absent=0"
+              }
+              className="font-semibold text-xl hover:text-[#812962] transition-colors ease-linear duration-100"
+            >
+              <MdOutlineAccessTime className="inline w-7 h-7 mb-1 mr-1" /> 9 a
+              15 hs.
+            </Link>
+            <Link
+              href={
+                "https://api.whatsapp.com/send/?phone=%2B542944959344&text=Hola%2C+tengo+una+consulta%21&type=phone_number&app_absent=0"
+              }
+              className="font-semibold text-xl hover:text-[#812962] transition-colors ease-linear duration-100"
+            >
+              <MdLocationPin className="inline w-7 h-7 mb-1 mr-1" /> Palacios
+              156 - 3° piso
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
